19150e99200cf5cbee50c5670fdd8b53c1b715b6
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-08 15:30:28 +0600

    javascript-class-2

---
+++ javascript2/index.js
@@ -0,0 +1,23 @@
+// let num = prompt("enter a number")
+
+// if (num % 5 === 0) {
+//     console.log(num, "is a multiple of 5");
+// }
+// else {
+//     console.log(num, "is not a multiple of 5");
+// }
+
+let score = prompt("enter your Score(0-100)")
+
+if (score >= 90 && score <= 100) {
+    console.log("Grade is A");
+}
+else if (score >= 70 && score <= 89) {
+    console.log("Grade is B");
+
+}
+else if (score >= 50 && score <= 69) {
+    console.log("Grade is C");
+
+}
+
