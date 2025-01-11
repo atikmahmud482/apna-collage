d15001ebc71c9966e15c9e33b7b75fdcaf60dc8d
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-11 18:19:23 +0600

    Tic Tac Toe Game Add

---
+++ javascript8/script.js
@@ -0,0 +1,11 @@
+let modeBtn = document.querySelector("#mood");
+let currMode = "light";
+modeBtn.addEventListener("click", () => {
+  if (currMode === "light") {
+    currMode = "dark";
+    document.querySelector("body").style.backgroundColor = "black";
+  } else {
+    currMode = "light";
+    document.querySelector("body").style.backgroundColor = "white";
+  }
+});
